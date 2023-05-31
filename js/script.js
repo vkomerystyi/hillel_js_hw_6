ab2ca1ea03ffa7a99343650516942c2f281d0132
Git start <v.komerystyi@gmail.com>
2023-05-31 21:56:26 +0300

    add change after code review

--- js/script.js
+++ js/script.js
@@ -55,9 +55,8 @@ alert("Sum is " + sum)
 const numbers = [5, 8, -1, "-6", 11, "ololo", "-2"]
 let sum = 0
 for (let i = 0; i < numbers.length; i++) {
-    if (Number.isNaN(parseInt(numbers[i]))) {
-        continue
+    if (!Number.isNaN(parseInt(numbers[i]))) {
+        sum += parseInt(numbers[i])
     }
-    sum += parseInt(numbers[i])
 }
 alert("Sum is " + sum)
